d9da71b1c4984f6703c8f8057044302d2d8b380f
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-09-18 15:58:41 +0900

    feat:second

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,79 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :check_post_ownership, only: %i[ edit update destroy ]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.page(params[:page]).per(10)
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "投稿を作成しました。" }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "投稿を更新しました。", status: :see_other }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: "投稿を削除しました。", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params.expect(:id))
+    end
+
+    # Check if the current user owns the post
+    def check_post_ownership
+      unless @post.user == current_user
+        redirect_to @post, alert: "この投稿を編集・削除する権限がありません。"
+      end
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.expect(post: [ :content ])
+    end
+end
