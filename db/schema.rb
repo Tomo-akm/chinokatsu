057206b3d4c5ebdf517fef856ccee19fc734e424
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-10-27 02:18:44 +0900

    chore: schemaを修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,10 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_23_143025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.bigint "post_id", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "post_tags", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id", "tag_id"], name: "index_post_tags_on_post_id_and_tag_id", unique: true
     t.index ["post_id"], name: "index_post_tags_on_post_id"
@@ -38,7 +28,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_23_143025) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -70,8 +60,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_23_143025) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "post_tags", "posts"
   add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "users"
